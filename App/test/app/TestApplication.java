8c13f7acb264c8d55195a1ba7843f1149046e777
ezeutno <ivan.suratno@gmail.com>
2021-05-10 15:49:43 +0700

    First test

---
+++ App/test/app/TestApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestApplication {
+
+    @Test
+    public void randomTestTrue() {
+        assertEquals(7, 7);
+    }
+}
