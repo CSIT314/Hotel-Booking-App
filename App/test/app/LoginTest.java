1615fcb239d7ac49b33cb31ce355a2ebe04163d4
ezeutno <ivan.suratno@gmail.com>
2021-05-20 14:12:57 +0700

    Add login test cases

---
+++ App/test/app/LoginTest.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginTest {
+
+    @Test
+    public void randomTestTrue() {
+        assertEquals(7, 7);
+    }
+}
