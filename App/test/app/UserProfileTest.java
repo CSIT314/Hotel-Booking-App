635599ae876266d2690671b2526a8f2a34f813a0
ARWA <52074173+ARWA-ALraddadi@users.noreply.github.com>
2021-05-28 00:18:43 +1000

    Update UserProfileTest.java

--- App/test/app/UserProfileTest.java
+++ App/test/app/UserProfileTest.java
@@ -68,38 +68,6 @@ public class UserProfileTest {
     public void CheckBookingsActionPerformedTest (){
          System.out.println(" Testing CheckBookingsActionPerforme()");
         
-         ResultSet RSet = getResult("SELECT * FROM booking_info WHERE username=\""+username+"\"");
-         model.setRowCount(0);
-              
-         try{
-         while(RSet.next()){
-
-          int Booking_ID = RSet.getInt("Booking_ID");
-          int rooms_confirmed = RSet.getInt("rooms_confirmed");
-          int rooms_waitlist = RSet.getInt("rooms_waitlist");
-          Date Date_In = RSet.getDate("Date_In");
-          Date Date_Out = RSet.getDate("Date_Out");
-          int status = RSet.getInt("Status");
-          int Hotel_ID = RSet.getInt("Hotel_ID");
-          String stat = null;
-          if(status==0){ stat="Confirmed";}
-          else if(status==1){stat="Waitlisted"; }
-          else if(status==2){stat="Cancelled"; }
-          Object row[] = {Booking_ID,rooms_confirmed,rooms_waitlist,Date_In,Date_Out,stat,Hotel_ID};
-          model.addRow(row);
-                
-         }
-         assertNotNull("Can't access Booking_ID for CheckBookingsActionPerformedTest ",Booking_ID);
-         assertNotNull("Can't access row for CheckBookingsActionPerformedTest ",row );
-         
-         Assert.assertNotNull(model);
-         
-         System.out.println("CheckBookingsActionPerformedTest Booking_ID successful");
-         System.out.println("CheckBookingsActionPerformedTest addRow successful");
-         }
-         catch (SQLException ex) {
-             ex.printStackTrace();
-        }
     
     }
     
