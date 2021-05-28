1fd4ecb9e4e31dc027746d56ec4ff46adf5c264b
petesN <pn407@uowmail.edu.au>
2021-05-28 10:39:03 +1000

    output format update

--- App/test/app/ModifyBookingTest.java
+++ App/test/app/ModifyBookingTest.java
@@ -90,6 +90,7 @@ public class ModifyBookingTest {
 
         return randdate;
     }
+    
 // testing jxdatepicker input and variables, username and booking id (BID)
     @Test
     public void testInputs() throws FileNotFoundException, ParseException {
@@ -100,7 +101,7 @@ public class ModifyBookingTest {
         int num = 10;
 
         for (int i = 0; i < num; i++) {
-            System.out.print("TEST CASE: " + i + "\n");
+            System.out.print("\n TEST CASE: " + i);
             username = generateAlphaNumeric(40);
             bid = generateRandomInt();
 
@@ -109,6 +110,7 @@ public class ModifyBookingTest {
             System.out.println("");
             System.out.println("USERNAME: " + username);
             System.out.println("BID: " + bid);
+            //get access to datepickers
             jXDatePicker1 = (JXDatePicker) TestUtils.getChildNamed(frame, "jXDatePicker1");
             jXDatePicker2 = (JXDatePicker) TestUtils.getChildNamed(frame, "jXDatePicker2");
 
@@ -118,7 +120,7 @@ public class ModifyBookingTest {
             // to see if each UI component is accessible
             System.out.println("Frame and DatePickers found");
             System.out.println("Test inputting dates in JXDatePicker");
-// integrating random dates into datepicker 
+            // integrating random dates into datepicker 
             Date dateIn = randDate();
             Date dateOut = randDate();
             jXDatePicker1.setDate(dateIn);
@@ -127,11 +129,14 @@ public class ModifyBookingTest {
             System.out.print("DATE OUT: " + jXDatePicker2.getDate() + "\n");
 
             System.out.print("");
+            // testing if values of username were correct and implemented correctly in frame variable
             assertEquals(username, frame.username);
+            // testing if values of bid were correct and implemented correctly in frame variable
             assertEquals(bid, frame.bid);
         }
     }
-// testing the buttion function in modifyBooking
+
+    // testing the buttion function in modifyBooking
     @Test
     public void testButtonFunction() {
         System.out.println("\n");
@@ -141,23 +146,30 @@ public class ModifyBookingTest {
         JXDatePicker jXDatePicker2;
         JButton button;
         int num = 10;
-        frame = new ModifyBooking(username, bid);
-        jXDatePicker1 = (JXDatePicker) TestUtils.getChildNamed(frame, "jXDatePicker1");
-        jXDatePicker2 = (JXDatePicker) TestUtils.getChildNamed(frame, "jXDatePicker2");
-        button = (JButton) TestUtils.getChildNamed(frame, "jButton1");
 
-        assertNotNull("jButton inaccessible", button);
-        assertNotNull("Frame component inaccessible", frame);
-        assertNotNull("Date in inaccessible", jXDatePicker1);
-        assertNotNull("Date out inaccessible", jXDatePicker2);
-        // to see if each UI component is accessible
-        System.out.println("Frame, Button and DatePickers found");
+        //test cases
         for (int i = 0; i < num; i++) {
+            // generate random username
+            username = generateAlphaNumeric(40);
+            //generate random bid
+            bid = generateRandomInt();
+
+            frame = new ModifyBooking(username, bid);
+            //get access to datepickers and button
+            jXDatePicker1 = (JXDatePicker) TestUtils.getChildNamed(frame, "jXDatePicker1");
+            jXDatePicker2 = (JXDatePicker) TestUtils.getChildNamed(frame, "jXDatePicker2");
+            button = (JButton) TestUtils.getChildNamed(frame, "jButton1");
+
+            assertNotNull("jButton inaccessible", button);
+            assertNotNull("Frame component inaccessible", frame);
+            assertNotNull("Date in inaccessible", jXDatePicker1);
+            assertNotNull("Date out inaccessible", jXDatePicker2);
+            // to see if each UI component is accessible
+            System.out.println("Frame, Button and DatePickers found");
+
             System.out.println("\n");
             System.out.print("TEST CASE: " + i + "\n");
             //randomised username and bid
-            username = generateAlphaNumeric(40);
-            bid = generateRandomInt();
             System.out.println("USERNAME: " + username);
             System.out.println("BID: " + bid);
             // integrating random dates into datepicker 
@@ -166,23 +178,30 @@ public class ModifyBookingTest {
             jXDatePicker1.setDate(dateIn);
             jXDatePicker2.setDate(dateOut);
             frame.setVisible(true);
+            // testing if values of username were correct and implemented correctly in frame variable
+            assertEquals(username, frame.username);
+            // testing if values of bid were correct and implemented correctly in frame variable
+            assertEquals(bid, frame.bid);
+
             // testing the code of button functionality
             try {
                 java.sql.Date inDate, outDate;
                 try {
                     inDate = convertDate(jXDatePicker1.getDate());
                     outDate = convertDate(jXDatePicker2.getDate());
+                    // testing if new checkIn and checkOut date are not null 
                     assertNotNull("No Booking Date checkIn", inDate);
                     assertNotNull("No booking Date checkOut", outDate);
                     System.out.print("DATE IN: " + jXDatePicker1.getDate() + "\n");
-
                     System.out.print("DATE OUT: " + jXDatePicker2.getDate() + "\n");
                 } catch (java.lang.ArrayIndexOutOfBoundsException | java.lang.NullPointerException e) {
                     JOptionPane.showMessageDialog(null, "Please fill in dates properly", "WARNING!", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
+                // check username and bid are not null 
                 assertNotNull("Username inaccessible", username);
                 assertNotNull("Booking ID inaccessible", bid);
+
                 InsertRow("UPDATE booking_info SET Status=2 WHERE Booking_ID=\"" + bid + "\";");
                 ResultSet rs = getResult("SELECT * FROM booking_info WHERE Booking_ID = " + bid);
                 if (rs.next()) {
@@ -249,7 +268,7 @@ public class ModifyBookingTest {
 
                     new UserProfile(username).setVisible(true);
                     frame.setVisible(true);
-                    // check if query was inserted
+                    // check if query was inserted and is not null
                     assertNotNull("Query did not update", query);
                 }
             } catch (SQLException ex) {
